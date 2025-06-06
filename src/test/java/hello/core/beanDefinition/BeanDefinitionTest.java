d89756c28cee0888e115423bc234a033b824b57a
ezzzzdev <dondjulee@gmail.com>
2025-06-06 11:30:48 +0900

    BeanDefinition으로 빈 설정 메타정보 확인

---
+++ src/test/java/hello/core/beanDefinition/BeanDefinitionTest.java
@@ -0,0 +1,28 @@
+package hello.core.beanDefinition;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class BeanDefinitionTest {
+
+    // BeanDefinition을 직접 보는 경우는 거의 없음, 추상화 구조만 알고 있기
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+//    GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+
+    @Test
+    @DisplayName("빈 설정 메타정보 확인")
+    void findApplicationBean(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION){
+                System.out.println("beanDefinitionName = " + beanDefinitionName + "beanDefinition = " + beanDefinition);
+            }
+        }
+    }
+}
