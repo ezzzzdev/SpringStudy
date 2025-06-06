edf0652a40aef3df0f1a0317a107a6f1769adcc2
ezzzzdev <dondjulee@gmail.com>
2025-06-06 10:21:40 +0900

    Add policy and AppConfig

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,26 +9,34 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 // 앱 전체를 설정 및 구성
 // AppConfig는 애플리케이션의 실제 동작에 필요한 구현 객체를 생성
 // AppConfig는 생성한 객체 인스턴스의 참조(래퍼런스)를 생성자를 통해서 주입(연결)해준다 = 의존성 주입(DI)
 // 이를 통해 생성자는 Config가 주입, Impl들은 실행에 집중하면 된다.
+@Configuration
 public class AppConfig {
 
+
     // AppConfig를 통해 memberService를 사용하면
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(MemberRepository());
     }
 
-    private MemberRepository MemberRepository() {
+    @Bean
+    public MemberRepository MemberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(MemberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
